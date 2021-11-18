4c3870caf957e66c1f95e2008b5d446297f55357
Michelle Ang <Michelle_Ang@simtech.a-star.edu.sg>
2021-11-18 14:35:10 +0800

    no message

--- Models/TodoItems.cs
+++ Models/TodoItems.cs
@@ -7,8 +7,8 @@
         public string Time { get; set; }
         public string PickUpPoint { get; set; }
         public string Destination { get; set; }
-        public long Current_Location_Latitude { get; set; }
-        public long Current_Location_Longitude { get; set; }
+        public double Current_Location_Latitude { get; set; }
+        public double Current_Location_Longitude { get; set; }
         public bool IsComplete { get; set; }
     }
 }
